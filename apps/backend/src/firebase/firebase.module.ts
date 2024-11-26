20d794d07a03db51b00ef05e7fd65c04202dc5e0
sunnithdev <sunnithdev@gmail.com>
2024-11-26 10:59:23 -0500

    CHANGES

--- apps/backend/src/firebase/firebase.module.ts
+++ apps/backend/src/firebase/firebase.module.ts
@@ -1,35 +1,50 @@
-import * as path from 'path';
 import * as admin from 'firebase-admin';
 import { Module } from '@nestjs/common';
 import * as dotenv from 'dotenv';
-import * as fs from 'fs';
 
 // Load environment variables
 dotenv.config();
+
+interface FirebaseServiceAccount {
+  type: string;
+  project_id: string;
+  private_key_id: string;
+  private_key: string;
+  client_email: string;
+  client_id: string;
+  auth_uri: string;
+  token_uri: string;
+  auth_provider_x509_cert_url: string;
+  client_x509_cert_url: string;
+  universe_domain: string;
+}
 
 @Module({
   providers: [
     {
       provide: 'FIREBASE_ADMIN',
       useFactory: async () => {
-        const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT_PATH;
-        console.log('Service Account Path:', serviceAccountPath); // Log the path
-        try {
-          const fileContents = fs.readFileSync(serviceAccountPath, 'utf8');
-          console.log('Raw File Contents:', fileContents);
-          
-          const serviceAccount = JSON.parse(fileContents);
-          console.log('Parsed Service Account:', serviceAccount);
-          
-          admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount),
-          });
+        
+        
+        const serviceAccount: FirebaseServiceAccount = {
+          type: process.env.FIREBASE_TYPE,
+          project_id: process.env.FIREBASE_PROJECT_ID,
+          private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+          private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+          client_email: process.env.FIREBASE_CLIENT_EMAIL,
+          client_id: process.env.CLIENT_ID,
+          auth_uri: process.env.auth_uri,
+          token_uri: process.env.token_uri,
+          auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+          client_x509_cert_url: process.env.client_x509_cert_url,
+          universe_domain:process.env.universe_domain
+        };
+        // Initialize Firebase Admin SDK
+        admin.initializeApp({
+          credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+        });
 
-          return admin;
-        } catch (error) {
-          console.error('Error reading/parsing service account:', error);
-          throw error;
-        }
+        return admin; // Firebase admin instance
       },
     },
   ],
